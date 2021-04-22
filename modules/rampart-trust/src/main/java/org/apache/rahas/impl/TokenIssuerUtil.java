e9c46778a0b517a45ed5b81613d9f745ff95e2b2
Rodrigo Yanis <rodrigoy@wso2.com>
2021-04-22 10:25:50 -0300

    Addressed dependency issues causing build failure

--- modules/rampart-trust/src/main/java/org/apache/rahas/impl/TokenIssuerUtil.java
+++ modules/rampart-trust/src/main/java/org/apache/rahas/impl/TokenIssuerUtil.java
@@ -19,6 +19,7 @@ import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.util.Base64;
 import org.apache.axis2.context.ConfigurationContext;
 import org.apache.axis2.context.MessageContext;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.rahas.Rahas;
